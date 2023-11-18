3f3fc6256d10896e4254ce5d2c643c991fa6dc01
malickkone <konemaodo@gmail.com>
2023-11-18 05:36:46 +0100

    updated

--- frontend/src/interfaces/components/home_cimetery.tsx
+++ frontend/src/interfaces/components/home_cimetery.tsx
@@ -4,27 +4,31 @@ import ba from "../../assets/bel-air.png"
 import sl from "../../assets/sain-lazarre.png"
 export const HomeCimetery = () => {
   return (
-        <div className="flex flex-col py-4 ">
-        <span className="text-red-500 text-3xl font-bold mx-auto">
-          Nos Cimetières
-        </span>
-        <span className="mx-auto">
-          {" "}
-          Les cimetières du Sénégal repertoriés dans notre base
-        </span>
-        <div className="grid grid-cols-3 gap-4 py-3  relative">
-          {[
-            { title: "Joal Fadiouth", lenght: 89, img:jf },
-            { title: "Sain-Lazarre", lenght: 89 , img:sl},
-            { title: "Bel-Air", lenght: 89, img:ba },
-          ].map((e) => (
-            <div className="bg-blue-500 h-44 flex flex-col relative">
-
-              <img src={e.img} className=' top-0 bottom-0 '/>
-              <span>{e.title}</span>
+    <div className="flex flex-col py-4 ">
+      <span className="text-red-500 text-3xl font-bold mx-auto">
+        Nos Cimetières
+      </span>
+      <span className="mx-auto">
+        {" "}
+        Les cimetières du Sénégal repertoriés dans notre base
+      </span>
+      <div className="grid grid-cols-3 gap-4 py-3  relative">
+        {[
+          { title: "Joal Fadiouth", lenght: 89, img: jf },
+          { title: "Saint-Lazarre", lenght: 89, img: sl },
+          { title: "Bel-Air", lenght: 89, img: ba },
+        ].map((e) => (
+          <div className=" h-44 ">
+            <div className='absolute '>
+              <img src={e.img} className=' top-0 bottom-0 ' />
             </div>
-          ))}
-        </div>
+            <div className=' flex flex-row justify-between pr-10 h-10 pt-2 pl-5 bg-white bg-opacity-40 relative -bottom-44'>
+              <span className='text-black font-extrabold'>{e.title}</span>
+              <button className="ring-1 h-5 ring-red-500 px-5 rounded-3xl text-black text-sm font-semibold">Découvrir</button>
+            </div>
+          </div>
+        ))}
       </div>
+    </div>
   )
 }
--- frontend/src/interfaces/components/home_wish.tsx
+++ frontend/src/interfaces/components/home_wish.tsx
@@ -1,27 +1,36 @@
 
 export const HomeWishes = () => {
   return (
-        <div className="flex flex-col gap-y-2 ">
-        <span className="text-red-500 text-3xl   font-bold mx-auto">
-          Envoyer une prière pour un proche
-        </span>
-        <div className="grid grid-cols-2 gap-x-4   h-52">
-                <div className='shadow-md h-full p-2 ring-inset ring-1 flex px-5 ring-slate-800/10 rounded-md'>
-                <span className="text-justify     m-auto">
+    <div className="flex flex-col gap-y-2 ">
+      <span className="text-red-500 text-3xl   font-bold mx-auto">
+        Envoyer une prière pour un proche
+      </span>
+      <div className="grid grid-cols-2 gap-x-4   h-52">
+        <div className=' h-full p-2  flex flex-col px-5  rounded-md'>
+          <span className="text-center  m-auto">
             Quand vous envoyez une prière sur notre site, un acte symbolique
             est posée pour honorer la mémoire des défunts pour la paix et le
             repos de leurs âmes. chaque dernier samedi du mois, un feu
             d’artfice est tiré au phare des mamelles avec l’ensemble des
             prières du mois redigées par nos visiteurs.
           </span>
-                </div>
-         
-          <div className="text-justify  shadow-md h-full  p-2 ring-inset ring-1 ring-slate-800/10 rounded-md"></div>
+          <div className="px-32">
+            <button className="ring-1 ring-red-500 p-2 rounded-3xl text-black text-sm font-semibold">Envoyer une prière gratuitement</button>
+          </div>
+
         </div>
-        <div className="flex ml-auto">
-            <button className="bg-red-500  p-2 rounded-md text-white">Envoyer une prière gratuitement</button>
+
+        <div className="text-center shadow-md h-full pb-5 px-11 ring-inset ring-1 ring-slate-800/10 rounded-2xl flex flex-col">
+          <span className="text-center text-xl m-auto">
+            “ Yasmina, je prie que ton ame repose en paix où qu’il soit. Tu nous manques et on te gardear à jamais dans nos coeurs pour l’étenité. ”
+          </span>
+          <span className="pl-72 text-zinc-600  ">
+            Ta chère maman
+          </span>
         </div>
- 
       </div>
+
+
+    </div>
   )
 }
